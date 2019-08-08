94487114dd4e302213af2c24f58136a7c8859543
cdallasanta <chris.dallasanta@gmail.com>
2019-08-08 12:18:08 -0700

    birdCards.js now working

--- backyard-birding-client/src/App.js
+++ backyard-birding-client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
+import birdCards from './images/birdCards.js';
+import wigeon from './images/birdCards/americanWigeon.png';
 import './App.css';
 
 function App() {
@@ -8,7 +10,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {
+            birdCards.map(bird => {
+              return <img src={bird.src} alt={bird.name} key={bird.id} />
+            })
+          }
         </p>
         <a
           className="App-link"
--- backyard-birding-client/src/images/birdCards.js
+++ backyard-birding-client/src/images/birdCards.js
@@ -1,9 +1,17 @@
+function importAll(r) {
+  let images = {};
+  r.keys().map(item => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('./birdCards', false, /\.png$/));
+
 const birdCards = [
-  { id: 1, src: './birdCards/americanCoot.png', name: 'American Coot', flightNums: [1, 2], season: "winter", habitat: ["wetland", "wildGrass"] },
-  { id: 2, src: './birdCards/americanCrow.png', name: 'American Crow', flightNums: [3, 4], season: "any", habitat: ["nuts", "lawn"] },
-  { id: 3, src: './birdCards/americanGoldfinch.png', name: 'American Goldfinch', flightNums: [5, 6], season: "spring", habitat: ["seeds", "trees"] },
-  { id: 4, src: './birdCards/americanRobin.png', name: 'American Robin', flightNums: [1, 2], season: "spring", habitat: ["insects", "lawn"] },
-  { id: 5, src: './birdCards/americanWigeon.png', name: 'American Wigeon', flightNums: [3,4], season: "fall", habitat: ["runningWater", "wildGrass"] }
+  { id: 1, src: images["americanCoot.png"], name: 'American Coot', flightNums: [1, 2], season: "winter", habitat: ["wetland", "wildGrass"] },
+  { id: 2, src: images["americanCrow.png"], name: 'American Crow', flightNums: [3, 4], season: "any", habitat: ["nuts", "lawn"] },
+  { id: 3, src: images["americanGoldfinch.png"], name: 'American Goldfinch', flightNums: [5, 6], season: "spring", habitat: ["seeds", "trees"] },
+  { id: 4, src: images["americanRobin.png"], name: 'American Robin', flightNums: [1, 2], season: "spring", habitat: ["insects", "lawn"] },
+  { id: 5, src: images["americanWigeon.png"], name: 'American Wigeon', flightNums: [3,4], season: "fall", habitat: ["runningWater", "wildGrass"] }
 ];
 
 export default birdCards;
