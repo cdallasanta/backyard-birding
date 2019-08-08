3347f14d0177d20106235e03ce3ed74980a409b8
cdallasanta <chris.dallasanta@gmail.com>
2019-08-08 12:28:08 -0700

    setting up redux

--- backyard-birding-client/src/App.js
+++ backyard-birding-client/src/App.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import birdCards from './images/birdCards.js';
-import wigeon from './images/birdCards/americanWigeon.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {
-            birdCards.map(bird => {
-              return <img src={bird.src} alt={bird.name} key={bird.id} />
-            })
-          }
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Made it!
     </div>
   );
 }
