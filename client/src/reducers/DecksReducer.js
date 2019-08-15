e8de4976b4ac524b3eb55de24792611e1d3423f8
cdallasanta <chris.dallasanta@gmail.com>
2019-08-14 20:55:44 -0700

    Source updated at 2019-08-14 20:55:44 -0700.

--- client/src/reducers/DecksReducer.js
+++ client/src/reducers/DecksReducer.js
@@ -24,9 +24,8 @@ let shuffle = function (array) {
 	return array;
 };
 
-// bird: shuffle([...birdCards]).slice(0,birdCards.length/4+1),
 const initState = {
-	bird: shuffle([...birdCards]).slice(0,2),
+	bird: shuffle([...birdCards]).slice(0,birdCards.length/3+1),
   backyard: shuffle([...initBackyardDeck])
 }
 
