41d6b49aa4dfdc3197936fdc338b278682b88e43
cdallasanta <chris.dallasanta@gmail.com>
2019-08-15 15:40:13 -0700

    perhaps it's all working now?

--- client/src/actions/GameActions.js
+++ client/src/actions/GameActions.js
@@ -29,7 +29,7 @@ export function showDice() {
 export function getGames() {
   return dispatch => {
     dispatch({type:"LOADING_SCORES"})
-    fetch('http://localhost:3001/api/high_scores')
+    fetch('/api/high_scores')
       .then(resp => resp.json())
       .then(scores => dispatch({type:"SET_SCORES", payload: scores}))
   }
