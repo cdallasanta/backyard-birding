09c339d139ebe899380c13fa228fe72e52043f2f
cdallasanta <chris.dallasanta@gmail.com>
2019-08-14 21:22:49 -0700

    Source updated at 2019-08-14 21:22:48 -0700.

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class NotFound extends Component {
   render () {
     return <div>
-      <h1>404: Not found</h1>
+      <h1>404: No birds here</h1>
       <a href='/'>Back to home</a>
     </div>
   }
