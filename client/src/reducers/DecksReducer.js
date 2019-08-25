6033ef0cad3f0796ee010a65f915018a0400696a
cdallasanta <chris.dallasanta@gmail.com>
2019-08-24 21:49:18 -0700

    birdcard ids fixed now without the duplicate wren

--- client/src/reducers/DecksReducer.js
+++ client/src/reducers/DecksReducer.js
@@ -25,7 +25,7 @@ let shuffle = function (array) {
 };
 
 const initState = {
-	bird: shuffle([...birdCards]).slice(0,birdCards.length/3+1),
+	bird: shuffle([...birdCards]).slice(0,birdCards.length/3),
   backyard: shuffle([...initBackyardDeck])
 }
 
