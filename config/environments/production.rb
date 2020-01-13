45c62dbfa72feabc7670e2ef9dcbcc1b6c3f32af
cdallasanta <chris.dallasanta@gmail.com>
2020-01-13 11:56:05 -0800

    adding files to help transfer to AWS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,9 @@ Rails.application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  # set secret key to env variable
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
