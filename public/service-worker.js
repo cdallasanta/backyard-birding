b7b4a6cdac03b5999553224ab0888f9d2e0b6929
cdallasanta <chris.dallasanta@gmail.com>
2020-01-15 15:26:38 -0800

    found the precompiling issue. Need to run > backyard-birding@1.0.0 postinstall /mnt/c/Users/Chris/documents/projects/backyard-birding > npm run build && npm run deploy && echo 'Client built!'
    > backyard-birding@1.0.0 build /mnt/c/Users/Chris/documents/projects/backyard-birding
    > cd client && npm install && npm run build && cd ..
    
    audited 902413 packages in 8.311s
    found 2 moderate severity vulnerabilities
      run `npm audit fix` to fix them, or `npm audit` for details
    
    > client@0.1.0 build /mnt/c/Users/Chris/documents/projects/backyard-birding/client
    > react-scripts build
    
    Creating an optimized production build...
    Compiled successfully.
    
    File sizes after gzip:
    
      58.63 KB  build/static/js/2.1a5b668e.chunk.js
      7.83 KB   build/static/js/main.11b89020.chunk.js
      1.1 KB    build/static/css/main.2a2a501d.chunk.css
      961 B     build/static/css/2.466c2356.chunk.css
      762 B     build/static/js/runtime~main.a8a9905a.js
    
    The project was built assuming it is hosted at the server root.
    You can control this with the homepage field in your package.json.
    For example, add this to build it for GitHub Pages:
    
      "homepage" : "http://myname.github.io/myapp",
    
    The build folder is ready to be deployed.
    You may serve it with a static server:
    
      npm install -g serve
      serve -s build
    
    Find out more about deployment here:
    
      https://bit.ly/CRA-deploy
    
    > backyard-birding@1.0.0 deploy /mnt/c/Users/Chris/documents/projects/backyard-birding
    > cp -a client/build/. public/
    
    Client built!

--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86c9087b244db0592eaa761f222bd331.js"
+  "/precache-manifest.2ca1c34d1815b4bcdb4e49d72796dc27.js"
 );
 
 self.addEventListener('message', (event) => {
