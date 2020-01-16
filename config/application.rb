e909f295cd9d55088273e6f89e351f2b1f09b175
cdallasanta <chris.dallasanta@gmail.com>
2020-01-15 17:21:04 -0800

    trying repacking assets

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,7 @@ module BackyardBirding
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.assets.prefix = '/some_other_path'
   end
 end
